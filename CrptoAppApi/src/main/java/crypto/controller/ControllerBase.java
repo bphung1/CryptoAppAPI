1b005763cc7c78aac5e94f9cfed9260378483277
Bao Phung <b.giaphung@gmail.com>
2021-12-29 10:28:08 -0600

    pom.xml

--- CrptoAppApi/src/main/java/crypto/controller/ControllerBase.java
+++ CrptoAppApi/src/main/java/crypto/controller/ControllerBase.java
@@ -5,6 +5,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("/api")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://cryptoappl.netlify.app")
 public class ControllerBase {
 }
